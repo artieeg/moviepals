f91ced50697d7d5f4eb9d88eb7ff7af58d3efed6
Artem <artmh@proton.me>
2023-08-01 23:23:43 +0300

    feat(fe-mobile): add trpc

--- apps/fe-mobile/src/App.tsx
+++ apps/fe-mobile/src/App.tsx
@@ -1,21 +1,25 @@
+import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { IconoirProvider } from "iconoir-react-native";
-import React from "react";
+
 import RootNavigator from "./navigators/RootNavigator";
+import { TRPCProvider } from "./utils/api";
 
 export function App() {
   return (
-    <IconoirProvider
-      iconProps={{
-        width: 32,
-        height: 32,
-        strokeWidth: 2,
-        color: "#0E0C10",
-      }}
-    >
-      <NavigationContainer>
-        <RootNavigator />
-      </NavigationContainer>
-    </IconoirProvider>
+    <TRPCProvider>
+      <IconoirProvider
+        iconProps={{
+          width: 32,
+          height: 32,
+          strokeWidth: 2,
+          color: "#0E0C10",
+        }}
+      >
+        <NavigationContainer>
+          <RootNavigator />
+        </NavigationContainer>
+      </IconoirProvider>
+    </TRPCProvider>
   );
 }
---
+++ apps/fe-mobile/src/utils/api.tsx
@@ -0,0 +1,89 @@
+import React from "react";
+import RNRestart from "react-native-restart";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { httpBatchLink } from "@trpc/client";
+import { createTRPCReact } from "@trpc/react-query";
+import superjson from "superjson";
+
+import { type AppRouter } from "@moviepals/api";
+
+import { env } from "./env";
+
+/**
+ * A set of typesafe hooks for consuming your API.
+ */
+export const api = createTRPCReact<AppRouter>();
+export { type RouterInputs, type RouterOutputs } from "@moviepals/api";
+
+/**
+ * Extend this function when going to production by
+ * setting the baseUrl to your production API URL.
+ */
+const getBaseUrl = () => {
+  console.log(env.API_BASE);
+
+  return env.API_BASE;
+};
+
+let authToken: string | undefined = undefined;
+export const AS_ACCESS_TOKEN_KEY = "@moviepals/access-token";
+
+export async function setAuthToken(token: string) {
+  await AsyncStorage.setItem(AS_ACCESS_TOKEN_KEY, token);
+
+  authToken = token;
+}
+
+export async function loadAuthToken() {
+  const token = await AsyncStorage.getItem(AS_ACCESS_TOKEN_KEY);
+
+  if (!token) {
+    return false;
+  } else {
+    authToken = token;
+
+    return true;
+  }
+}
+
+export async function signOut() {
+  await AsyncStorage.removeItem(AS_ACCESS_TOKEN_KEY);
+  authToken = undefined;
+
+  RNRestart.restart();
+}
+
+/**
+ * A wrapper for your app that provides the TRPC context.
+ * Use only in _app.tsx
+ */
+
+export function TRPCProvider(props: { children: React.ReactNode }) {
+  const [queryClient] = React.useState(() => new QueryClient());
+  const [trpcClient] = React.useState(() =>
+    api.createClient({
+      transformer: superjson,
+      links: [
+        httpBatchLink({
+          url: `${getBaseUrl()}/trpc`,
+          headers() {
+            return authToken
+              ? {
+                  Authorization: `Bearer ${authToken}`,
+                }
+              : {};
+          },
+        }),
+      ],
+    })
+  );
+
+  return (
+    <api.Provider client={trpcClient} queryClient={queryClient}>
+      <QueryClientProvider client={queryClient}>
+        {props.children}
+      </QueryClientProvider>
+    </api.Provider>
+  );
+}
