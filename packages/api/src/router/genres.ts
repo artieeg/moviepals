c6b46f2e63f2112cf5dcc97ff72e53f8669c3818
Artem <artmh@proton.me>
2023-08-06 12:12:19 +0300

    fix(api): remove unused procedure

--- packages/api/src/router/genres.ts
+++ packages/api/src/router/genres.ts
@@ -4,33 +4,6 @@ import { genres as genreDefinitions } from "../genres";
 import { createTRPCRouter, protectedProcedure } from "../trpc";
 
 export const genres = createTRPCRouter({
-  toggleAllGenres: protectedProcedure.mutation(async ({ ctx }) => {
-    const genres = await ctx.prisma.enabledGenre.findMany({
-      where: { userId: ctx.user },
-    });
-
-    const enabledGenres = await ctx.prisma.enabledGenre.findMany({
-      where: {
-        userId: ctx.user,
-      },
-    });
-
-    const enabledGenreIds = enabledGenres.map(
-      (enabledGenre) => enabledGenre.genreId,
-    );
-
-    const genresToEnable = genres.filter(
-      (genre) => !enabledGenreIds.includes(genre.genreId),
-    );
-
-    await ctx.prisma.enabledGenre.createMany({
-      data: genresToEnable.map((genre) => ({
-        userId: ctx.user,
-        genreId: genre.genreId,
-      })),
-    });
-  }),
-
   toggleGenre: protectedProcedure
     .input(z.object({ genre: z.number(), enabled: z.boolean() }))
     .mutation(async ({ ctx, input: { genre, enabled } }) => {
