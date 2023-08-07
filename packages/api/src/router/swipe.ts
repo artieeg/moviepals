4467b86687ff7ba5e9282734d8c3719d3b3f87b3
Artem <artmh@proton.me>
2023-08-07 08:51:47 +0300

    fix(db): remove the swipes table

---
+++ packages/api/src/router/swipe.ts
@@ -0,0 +1,4 @@
+import {createTRPCRouter} from "../trpc";
+
+export const swipe = createTRPCRouter({
+})
--- packages/api/src/services/cache.ts
+++ packages/api/src/services/cache.ts
@@ -1,17 +1,13 @@
 //TODO: replace with redis
 const cache = new Map();
 
-export function cachify(fn: Function) {
-  return async function (...args: unknown[]) {
-    console.log(args);
+export function cachify<T extends (...args: any[]) => any>(fn: T) {
+  return async function (...args: Parameters<T>) {
     const key = JSON.stringify(args);
 
     if (cache.has(key)) {
-      console.log("cache hit");
       return cache.get(key);
     }
-
-    console.log("running");
 
     const result = await fn(...args);
 
