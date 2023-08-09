207f66ff6534d54ff8f0f2ea4c2fbf93847d11c0
Artem <artmh@proton.me>
2023-08-09 23:24:19 +0300

    feat(api): add swipe and movie_feed routesr

--- packages/api/src/router/root.ts
+++ packages/api/src/router/root.ts
@@ -2,7 +2,9 @@ import { createTRPCRouter } from "../trpc";
 import { connection } from "./connection";
 import { connection_requests } from "./connection_requests";
 import { genres } from "./genres";
+import { movie_feed } from "./movie_feed";
 import { streaming_service } from "./streaming_service";
+import { swipe } from "./swipe";
 import { user } from "./user";
 
 export const appRouter = createTRPCRouter({
@@ -11,6 +13,8 @@ export const appRouter = createTRPCRouter({
   genres,
   connection,
   connection_requests,
+  swipe,
+  movie_feed,
 });
 
 // export type definition of API
