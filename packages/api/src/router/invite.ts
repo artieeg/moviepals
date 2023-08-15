37940d2eb960d9d9661cc03480a57ab0e7912e4d
Artem <artmh@proton.me>
2023-08-16 00:29:30 +0300

    feat(api): apply invites

--- packages/api/src/router/invite.ts
+++ packages/api/src/router/invite.ts
@@ -1,8 +1,38 @@
 import { TRPCError } from "@trpc/server";
+import { z } from "zod";
 
 import { createTRPCRouter, protectedProcedure } from "../trpc";
 
 export const invite = createTRPCRouter({
+  applyInvite: protectedProcedure
+    .input(
+      z.object({
+        inviteUrl: z.string(),
+      }),
+    )
+    .mutation(async ({ ctx, input: { inviteUrl } }) => {
+      const inviteLink = await ctx.prisma.userInviteLink.findUnique({
+        where: { slug: inviteUrl },
+      });
+
+      const inviter = await ctx.prisma.user.findUnique({
+        where: { userInviteLinkId: inviteLink?.id },
+      });
+
+      if (!inviter) {
+        throw new TRPCError({ code: "NOT_FOUND" });
+      }
+
+      await ctx.prisma.connection.create({
+        data: {
+          firstUserId: inviter.id,
+          secondUserId: ctx.user,
+        },
+      });
+
+      return {inviter};
+    }),
+
   fetchInviteUrl: protectedProcedure.query(async ({ ctx }) => {
     const user = await ctx.prisma.user.findUnique({
       where: { id: ctx.user },
