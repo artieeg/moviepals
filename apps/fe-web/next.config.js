b7230ab865c07e3a7c7f9463e94c2d7f62ca0daf
Artem <artmh@proton.me>
2023-08-30 14:23:58 +0300

    handle redirects

--- apps/fe-web/next.config.js
+++ apps/fe-web/next.config.js
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  async redirects() {
+    return [
+      {
+        source: "/testflight",
+        destination: "https://testflight.apple.com/join/riAOMve5",
+        permanent: false,
+        basePath: false,
+      },
+    ];
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
