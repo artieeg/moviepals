0e093e19ffcb84f6ad5c1aeec5e5e12fb173cfd6
Artem <artmh@proton.me>
2023-09-02 23:43:08 +0300

    fix(api): increase # of movies in feed

--- packages/api/src/router/movie_feed.ts
+++ packages/api/src/router/movie_feed.ts
@@ -17,10 +17,10 @@ import { getMovies, GetMoviesParams } from "../services";
 import { Context, createTRPCRouter, protectedProcedure } from "../trpc";
 
 /** Number of movies that we return to the client */
-const MOVIES_PER_PAGE = 24;
+const MOVIES_PER_PAGE = 30;
 
 /** Max number of movies that we mix in from friend swipes */
-const MIX_IN_MOVIES_COUNT = 12;
+const MIX_IN_MOVIES_COUNT = 15;
 
 const getMovieFeedInput = z.object({
   start_year: z.number().min(1960).max(2019).optional(),
