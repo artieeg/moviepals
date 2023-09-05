f5511b7ea4ffd12b751357c81f4bdbd55f61543b
Artem <artmh@proton.me>
2023-09-05 22:17:53 +0300

    feat(fe-mobile): handle push notifications

--- apps/fe-mobile/index.js
+++ apps/fe-mobile/index.js
@@ -3,10 +3,14 @@
  */
 
 import { AppRegistry, LogBox } from "react-native";
+import messaging from "@react-native-firebase/messaging";
 
 LogBox.ignoreAllLogs();
 
 import { name as appName } from "./app.json";
 import { App } from "./src/App";
+import { handleRemoteMessage } from "./src/utils/handleRemoteMessage";
+
+messaging().setBackgroundMessageHandler(handleRemoteMessage);
 
 AppRegistry.registerComponent(appName, () => App);
