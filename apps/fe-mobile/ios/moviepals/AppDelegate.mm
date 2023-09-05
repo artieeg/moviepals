8420522e1fc99dede83fae22959015a9c588d814
Artem <artmh@proton.me>
2023-09-05 22:17:53 +0300

    feat(fe-mobile): handle push notifications

--- apps/fe-mobile/ios/moviepals/AppDelegate.mm
+++ apps/fe-mobile/ios/moviepals/AppDelegate.mm
@@ -2,6 +2,9 @@
 #import <Firebase.h>
 #import "RNFBMessagingModule.h"
 
+#import <UserNotifications/UserNotifications.h>
+#import <RNCPushNotificationIOS.h>
+
 #import <React/RCTLinkingManager.h>
 #import <React/RCTBundleURLProvider.h>
 
@@ -30,8 +33,41 @@
   self.initialProps = [RNFBMessagingModule addCustomPropsToUserProps:nil withLaunchOptions:launchOptions];
 
   [FIRApp configure];
+
+  UNUserNotificationCenter *center = [UNUserNotificationCenter currentNotificationCenter];
+  center.delegate = self;
   
   return [super application:application didFinishLaunchingWithOptions:launchOptions];
+}
+
+//Called when a notification is delivered to a foreground app.
+-(void)userNotificationCenter:(UNUserNotificationCenter *)center willPresentNotification:(UNNotification *)notification withCompletionHandler:(void (^)(UNNotificationPresentationOptions options))completionHandler
+{
+  completionHandler(UNNotificationPresentationOptionSound | UNNotificationPresentationOptionAlert | UNNotificationPresentationOptionBadge);
+}
+
+// Required for the register event.
+- (void)application:(UIApplication *)application didRegisterForRemoteNotificationsWithDeviceToken:(NSData *)deviceToken
+{
+ [RNCPushNotificationIOS didRegisterForRemoteNotificationsWithDeviceToken:deviceToken];
+}
+// Required for the notification event. You must call the completion handler after handling the remote notification.
+- (void)application:(UIApplication *)application didReceiveRemoteNotification:(NSDictionary *)userInfo
+fetchCompletionHandler:(void (^)(UIBackgroundFetchResult))completionHandler
+{
+  [RNCPushNotificationIOS didReceiveRemoteNotification:userInfo fetchCompletionHandler:completionHandler];
+}
+// Required for the registrationError event.
+- (void)application:(UIApplication *)application didFailToRegisterForRemoteNotificationsWithError:(NSError *)error
+{
+ [RNCPushNotificationIOS didFailToRegisterForRemoteNotificationsWithError:error];
+}
+// Required for localNotification event
+- (void)userNotificationCenter:(UNUserNotificationCenter *)center
+didReceiveNotificationResponse:(UNNotificationResponse *)response
+         withCompletionHandler:(void (^)(void))completionHandler
+{
+  [RNCPushNotificationIOS didReceiveNotificationResponse:response];
 }
 
 - (NSURL *)sourceURLForBridge:(RCTBridge *)bridge
--- apps/fe-mobile/src/utils/handleRemoteMessage.ts
+++ apps/fe-mobile/src/utils/handleRemoteMessage.ts
@@ -1,15 +1,80 @@
+import { Linking } from "react-native";
+import PushNotification, {
+  ChannelObject,
+  Importance,
+} from "react-native-push-notification";
+import PushNotificationIOS from "@react-native-community/push-notification-ios";
 import { FirebaseMessagingTypes } from "@react-native-firebase/messaging";
 import { z } from "zod";
 
 const zRemoteMessageDataSchema = z.object({
   title: z.string(),
   body: z.string(),
-  route: z.string(),
-  extra: z.unknown(),
+  bigText: z.string().optional(),
+  link: z.string().optional(),
+  sound: z.boolean().default(false),
 });
 
 export type RemoteMessageData = z.infer<typeof zRemoteMessageDataSchema>;
 
-export function handleRemoteMessage(
+export async function handleRemoteMessage(
   message: FirebaseMessagingTypes.RemoteMessage,
-) {}
+) {
+  const data = zRemoteMessageDataSchema.parse(message.data);
+  console.log(data);
+
+  PushNotification.createChannel(
+    {
+      channelId: "channel",
+      channelName: "MoviePals",
+      channelDescription: "MoviePals notifications",
+      playSound: false,
+      soundName: "default",
+      importance: Importance.HIGH,
+      vibrate: true,
+    },
+    () => {},
+  );
+
+  PushNotification.localNotification({
+    channelId: "channel",
+    smallIcon: "ic_notification",
+    bigText: data.bigText,
+    subText: data.body,
+    bigLargeIcon: "ic_launcher",
+    vibrate: false,
+
+    title: data.title,
+    message: data.body,
+    userInfo: {
+      link: data.link,
+    },
+    invokeApp: false,
+    playSound: data.sound,
+    soundName: "default",
+  });
+}
+
+PushNotification.configure({
+  onNotification: function (notification) {
+    if (notification.data.link) {
+      setTimeout(() => {
+        Linking.openURL(notification.data.link);
+      }, 1000);
+    }
+
+    notification.finish(PushNotificationIOS.FetchResult.NoData);
+  },
+
+  onRegistrationError: function (err) {
+    console.error(err.message, err);
+  },
+
+  permissions: {
+    alert: true,
+    badge: true,
+    sound: true,
+  },
+
+  popInitialNotification: true,
+});
